5e3ca90bbc07b676a7cd028d7af3ac1f82ad3a65
Claudia Lisboa <ccalumby@gmail.com>
2023-01-28 19:30:21 -0500

    add PostContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {  createContext, useState } from "react";
+import React, {  createContext, useState, useContext, useReducer } from "react";
 import Login from './components/Loging'
 import Header from "./components/Header"
 import CreatePost from "./components/CreatePost"
@@ -8,24 +8,27 @@ import  './App.css'
 
 
 export const UserContext = createContext();
-
-
+export const PostContext = createContext({
+  posts:[]
+});
 
 function App() {
+  const initialPostState = useContext(PostContext);
+  const [state, dispatch] = useReducer(()=> {}, initialPostState);
   const [user, setUser] = useState(""); // will display user login when user public the post
-  const [posts, setPosts] = useState([]);
+  //const [posts, setPosts] = useState([]);
  
 
   React.useEffect(() => {
     document.title = user ? `${user}'s Feed` : "Please login";
   }, [user]);
 
-  const handleAddPost = React.useCallback(
+  /*const handleAddPost = React.useCallback(
     newPost => {
       setPosts([newPost, ...posts]);
     },
     [posts]
-  );
+  );*/
 
 
 
@@ -33,12 +36,15 @@ function App() {
     return <Login setUser={setUser} />;
   }
   return (
-    <UserContext.Provider value={user}>
-      <Header user={user} setUser={setUser}/>
-      <CreatePost user={user} handleAddPost={handleAddPost} />
-      <PostList  posts={posts} />
-      
-    </UserContext.Provider>
+    <PostContext.Provider value ={{ state, dispatch }}>
+        <UserContext.Provider value={user}>
+          <Header user={user} setUser={setUser}/>
+          <CreatePost user={user} 
+          //handleAddPost={handleAddPost} 
+          />
+          <PostList  posts={state.posts} />
+        </UserContext.Provider>
+    </PostContext.Provider>
   );
 }
 
